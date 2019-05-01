e22a938045d534e736255f3a56efe1ec2d08330e
NikitaIgorBondarenko <47746847+NikitaIgorBondarenko@users.noreply.github.com>
2019-05-01 16:01:47 +0200

    Model project

--- modelproject/modelproject/code2.py
+++ modelproject/modelproject/code2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.optimize import minimize
 
 
 
@@ -14,3 +14,24 @@ def objective(x) :
 
 x1=[2,2,2]
 print(objective(x1))
+
+def constraint1(x):
+    return x[0]*x[1]*x[2]-25
+def constraint2(x):
+    sum_consume= 40
+    for i in range(3) :
+        sum_consume= sum_consume-x[i]**2
+    return sum_consume
+
+b=(1.0, 1.0)
+bds=(b,b,b)
+con1={'type':'ineq', 'fun':constraint1}
+con2={'type':'eq', 'fun':constraint2}
+cons=[con1, con2]
+
+sol=minimize(objective, x1, method='SLSQP', \
+    bounds=bds, constraints=cons)
+
+print(sol)
+
+print(sol.x[1])
