78d799e992f42aac4ccb3db7fd5f54691bfe900e
NikitaIgorBondarenko <47746847+NikitaIgorBondarenko@users.noreply.github.com>
2019-05-01 15:42:24 +0200

    commit Model

--- modelproject/code2.py
+++ modelproject/code2.py
@@ -1,1 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+import time
+from scipy import linalg
+from scipy import optimize
+import sympy as sm
 
+
+
+def objective(x) :
+    x1 = x[0]
+    x2 = x[1]
+    x3 = 
